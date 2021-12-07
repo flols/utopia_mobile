8fac664b8c6ea8465eb1f0503a9682f3b7e185fd
Florian Le Saint <florian.lesaint@brest-opencampus.com>
2021-12-07 22:54:30 +0100

    Firebase Authentication class email

---
+++ lib/home/login_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:utopia_mobile/shared/widgets/constants.dart';
+import 'package:utopia_mobile/shared/widgets/loading.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final _formkey = GlobalKey<FormState>();
+  String error = '';
+  bool loading = false;
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool showSignIn = true;
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  void toggleView() {
+    setState(() {
+      _formkey.currentState?.reset();
+      error = '';
+      emailController.text = '';
+      passwordController.text = '';
+      showSignIn = !showSignIn;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      loading ?
+      Loading() :
+      Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          elevation: 0.0,
+          title: Text(showSignIn
+              ? 'Sign in to Utopia'
+              : 'Register to Utopia'),
+          actions: <Widget> [
+            TextButton.icon(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+                label: Text(showSignIn ? 'Sign In' : "Register",
+                  style: TextStyle(color: Colors.white)),
+                onPressed: () => toggleView(),
+            ),
+          ],
+        ),
+        body: Container(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 30.0),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: emailController,
+                  decoration: textInputDecoration.copyWith(hintText: "Email"),
+                  validator: (value) => value!.isEmpty ? "Enter an email" : null,
+                ),
+                SizedBox(height: 10.0),
+                TextFormField(
+                  controller: passwordController,
+                  decoration: textInputDecoration.copyWith(hintText: "Password"),
+                  obscureText: true,
+                  validator: (value) => value.length < 4 ? "Enter password with at least 4 characters" : null,
+                ),
+                SizedBox(height: 10.0),
+                ElevatedButton(
+                  child: Text(
+                    showSignIn ? "Sign In" : "Register",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    if (_formkey.currentState.validate()) {
+                      setState(() => loading = true);
+                      var password = passwordController.value.text;
+                      var email = emailController.value.text;
+
+                      dynamic result = null;
+                      if (result == null) {
+                        setState(() {
+                          loading = false;
+                          error = 'Please supply a valid email!';
+                        });
+                      }
+                    }
+                  },
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red, fontSize: 15.0),
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+
+  }
+}
---
+++ lib/shared/widgets/services/authentication.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:utopia_mobile/models/user.dart';
+
+class Authentication {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  AppUser _userFromFirebaseUser(User user) {
+    return user != null ? AppUser(uid: user.uid) : null;
+  }
+
+  Stream<AppUser> get user {
+    return _auth.authStateChanges().map(_userFromFirebaseUser);
+  }
+
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result =
+        await _auth.signInWithEmailAndPassword(email: email, password: password);
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch(exception) {
+      print(exception.toString());
+      return null;
+    }
+  }
+
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result =
+        await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch(exception) {
+      print(exception.toString());
+      return null;
+    }
+  }
+
+
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    }
+    catch(exception) {
+      print(exception.toString());
+      return null;
+    }
+  }
+}
